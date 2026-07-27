bff0164a6c36f5b180324b881530430b75cae7e8
Prakhar Sharma <129146499+Prakhar-246@users.noreply.github.com>
2026-07-27 20:16:10 +0530

    Time: 0 ms (100%), Space: 19.8 MB (81.38%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,12 +1,50 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        nums2 = nums
+        # nums2 = nums
 
-        for i in range (len(nums)):
-            if target not in nums:
-                nums2 = list.copy(nums)
-                nums2.append(target)
-                nums2.sort()
-        for j in range(len(nums2)):
-            if nums2[j] == target:
-                return j
+        # for i in range (len(nums)):
+        #     if target not in nums:
+        #         nums2 = list.copy(nums)
+        #         nums2.append(target)
+        #         nums2.sort()
+        # for j in range(len(nums2)):
+        #     if nums2[j] == target:
+        #         return j
+        # low = 0 
+        # high = len(nums) - 1
+
+        # while low <= high:
+        #     mid = (low+high) // 2
+        #     if nums[mid] == target :
+        #         return mid
+        #     elif nums[mid] > target: 
+        #         high = mid - 1
+        #     else :
+        #         low = mid + 1
+        # if mid == 0 :
+        #     return 0
+        # else :
+        #     return high-1
+        # n = len(nums)
+        # ind = -1
+        # for i in range (0,n):
+        #     if target == 0:
+        #         return 0
+        #     elif nums [i] == target:
+        #         ind  = i
+        #     elif nums[i] < target:
+        #         ind = i+1
+        # return ind
+        n = len(nums)
+        low = 0
+        lb = n
+        high = n -1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] >= target:
+                lb = mid
+                high = mid-1
+            else :
+                low = mid + 1
+        return lb
